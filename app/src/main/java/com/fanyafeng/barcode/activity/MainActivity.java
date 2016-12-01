fb4ede303d88d4fbf620482bea58a6155856c94b
fanyafeng <fanyafeng@365rili.com>
2016-12-01 14:54:50 +0800

    利用zxing生成二维码

--- app/src/main/java/com/fanyafeng/barcode/activity/MainActivity.java
+++ app/src/main/java/com/fanyafeng/barcode/activity/MainActivity.java
@@ -1,17 +1,24 @@
 package com.fanyafeng.barcode.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.fanyafeng.barcode.R;
 import com.fanyafeng.barcode.BaseActivity;
+import com.fanyafeng.barcode.util.QRCode;
 
 //需要搭配baseactivity，这里默认为baseactivity,并且默认Baseactivity为包名的根目录
 public class MainActivity extends BaseActivity {
+    private ImageView ivBarCode;
+    private ImageView ivLogoBarCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +35,26 @@ public class MainActivity extends BaseActivity {
 
     //初始化UI空间
     private void initView() {
-
+        ivBarCode = (ImageView) findViewById(R.id.ivBarCode);
+        ivLogoBarCode = (ImageView) findViewById(R.id.ivLogoBarCode);
     }
 
     //初始化数据
     private void initData() {
+        Bitmap bitmap = QRCode.createQRCode("http://news.ifeng.com/a/20161201/50345907_0.shtml", 500);
+        ivBarCode.setImageBitmap(bitmap);
 
+        Bitmap bitmap1 = QRCode.createQRCodeWithLogo("http://news.ifeng.com/a/20161201/50345907_0.shtml", 500, BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+        ivLogoBarCode.setImageBitmap(bitmap1);
     }
 
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btnScanCode:
+                startActivity(new Intent(this, ScanCodeActivity.class));
+                break;
+        }
+    }
 }
